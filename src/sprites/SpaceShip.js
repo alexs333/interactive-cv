d3db44ac5a4baf43cc4f4a80add3551d2765bb7d
Alex Serdyuk <aserdyuk@dius.com.au>
2019-05-15 11:55:35 +1000

    feat: added debouncing on touch with ship

--- src/sprites/SpaceShip.js
+++ src/sprites/SpaceShip.js
@@ -4,6 +4,7 @@ export default class SpaceShip extends GameObjects.Container {
   constructor (config) {
     super(config.scene, config.x, config.y, config.children)
     this.noOfSkills = 3
+    this.touchable = true
 
     config.scene.physics.world.enable(this)
     config.scene.add.existing(this)
@@ -19,11 +20,16 @@ export default class SpaceShip extends GameObjects.Container {
   }
 
   touch () {
-    if (this.noOfSkills > 0) {
-      this.noOfSkills--
-      this.scene.sound.play('touchSuccess', { volume: 2 })
-    } else {
-      this.scene.sound.play('touchEmpty')
+    if (this.touchable) {
+      if (this.noOfSkills > 0) {
+        this.noOfSkills--
+        this.scene.sound.play('touchSuccess', { volume: 2 })
+      } else {
+        this.scene.sound.play('touchEmpty')
+      }
+
+      this.touchable = false
+      this.scene.time.delayedCall(700, function () { this.touchable = true }, null, this)
     }
   }
 }
